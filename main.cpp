51dfd55c4ffe6d4a31a2bd055dc94c12dd178661
miheyinfo <miheyinfo@gmail.com>
2020-10-08 20:38:50 +0200

    default(none)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
     printf("Number of Processors: %d\n", omp_get_num_procs());
 
-    #pragma omp parallel num_threads(2)
+    #pragma omp parallel num_threads(2) default(none)
     for (int i = 0; i < 4; ++i) {
         printf("Hello from Thread %d, i: %d \n", omp_get_thread_num(), i);
     }
