d4dca6dd6237505c5e062be9bec84bdfeb382b49
Switchwire <henverx3@gmail.com>
2020-10-08 20:57:19 +0200

    update for more thread usage

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 int main() {
     printf("Number of Processors: %d\n", omp_get_num_procs());
 
-    #pragma omp parallel num_threads(2) default(none)
-    for (int i = 0; i < 4; ++i) {
-        printf("Hello from Thread %d, i: %d \n", omp_get_thread_num(), i);
+    #pragma omp parallel num_threads(55) default(none)
+    for (int i = 0; i < 400000; ++i) {
+        if(i%10 == 0){
+            printf("Hello from Thread %d, i: %d \n", omp_get_thread_num(), i);
+        }
+
     }
 
     return 0;
