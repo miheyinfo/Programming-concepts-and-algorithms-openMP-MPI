3c3526859e715b818541e8ddedb43b7d10bf6e25
miheyinfo <miheyinfo@gmail.com>
2020-10-08 20:31:58 +0200

    add openMP

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <omp.h>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    printf("Number of Processors: %d\n", omp_get_num_procs());
+
+    #pragma omp parallel num_threads(2)
+    for (int i = 0; i < 4; ++i) {
+        printf("Hello from Thread %d, i: %d \n", omp_get_thread_num(), i);
+    }
+
     return 0;
 }
