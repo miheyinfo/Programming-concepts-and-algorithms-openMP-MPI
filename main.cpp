954896d0ae6159b93612b3ecc0d53b5fa879aca9
miheyinfo <miheyinfo@gmail.com>
2020-10-09 22:13:37 +0200

    add grayscale

--- main.cpp
+++ main.cpp
@@ -7,27 +7,41 @@ using namespace cv;
 
 
 int main() {
-//    printf("Number of Processors: %d\n", omp_get_num_procs());
-//
-//    #pragma omp parallel for num_threads(55)
-//    for (int i = 0; i < 400000; ++i) {
-//        if(i%10 == 0){
-//            printf("Hello from Thread %d, i: %d \n", omp_get_thread_num(), i);
-//        }
-//
-//    }
-
-
+    
     // Search Lenna
     auto pathToLenna = std::filesystem::current_path().string().append("/../lenna.png");
     // Read the image file
     Mat sourceImage = imread(pathToLenna);
     // Check for failure
-    if (sourceImage.empty())
-    {
+    if (sourceImage.empty()) {
         printf("Could not open or find the image");
         return -1;
     }
+
+    unsigned long width = sourceImage.cols;
+    unsigned long height = sourceImage.rows;
+
+    // create blank grayscale Mat image object
+    Mat grayscale(height, width, CV_8U, Scalar(0));
+
+    #pragma omp parallel for collapse(2) default(none) shared(width, height, sourceImage, grayscale)
+    for (int i = 0; i < width; ++i) {
+        for (int j = 0; j < height; ++j) {
+
+            auto pixel = sourceImage.at<Vec3b>(i,j);
+
+            unsigned char blue = pixel[0];
+            unsigned char red = pixel[1];
+            unsigned char green = pixel[2];
+
+            unsigned char greyScaleColor = red * 0.21 + green * 0.72 + blue * 0.07;
+
+            grayscale.at<unsigned char>(i,j) = saturate_cast<unsigned char>(greyScaleColor);
+        }
+    }
+
+    namedWindow("Grayscale", 1);
+    imshow("Grayscale", grayscale);
 
     namedWindow("Original Image", 1);
     imshow("Original Image", sourceImage);
